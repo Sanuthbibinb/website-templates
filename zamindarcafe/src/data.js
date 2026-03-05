08a348859f23f14acf1b1a6cf939be8a172b7d95
Sanuth Bibin.B <165442353+Sanuthb@users.noreply.github.com>
2026-03-05 10:38:00 +0530

    website done

---
+++ zamindarcafe/src/data.js
@@ -0,0 +1,130 @@
+export const menuCategories = [
+  {
+    id: 'teas',
+    name: "Classic Tea's",
+    items: [
+      { name: "Black Tea", price: 25 },
+      { name: "Ginger Tea", price: 20 },
+      { name: "Elaichi Tea", price: 20 },
+      { name: "Badham Tea", price: 20 },
+      { name: "Sonti Chai", price: 20 },
+      { name: "Masala Tea", price: 20 },
+      { name: "Lemon Tea", price: 20 },
+      { name: "Green Tea", price: 20 },
+      { name: "Subja Lemon Tea", price: 20 },
+    ]
+  },
+  {
+    id: 'coffee',
+    name: "Coffee & More",
+    items: [
+      { name: "Black Coffee", price: 20 },
+      { name: "Chocolate Coffee", price: 25 },
+      { name: "Sonti Coffee", price: 25 },
+      { name: "Dark Hot Coffee", price: 35 },
+      { name: "Cold Coffee", price: 55 },
+    ]
+  },
+  {
+    id: 'herbal',
+    name: "Herbal & Special Tea's",
+    items: [
+      { name: "Ashwagandha Tea", price: 20 },
+      { name: "Korean Ginseng Tea", price: 30 },
+      { name: "Chocolate Blast Tea", price: 79 },
+    ]
+  },
+  {
+    id: 'milk',
+    name: "Flavored Milk",
+    items: [
+      { name: "Rose Milk", price: 20 },
+      { name: "Boost", price: 20 },
+      { name: "Horlicks", price: 20 },
+      { name: "Bournvita", price: 20 },
+      { name: "Pista Badam", price: 20 },
+      { name: "Pepper Milk", price: 20 },
+      { name: "Chilled Badam Milk", price: 55 },
+    ]
+  },
+  {
+    id: 'thick-shakes',
+    name: "Thick Shakes",
+    items: [
+      { name: "Vanilla Shake", price: 99 },
+      { name: "Strawberry Shake", price: 99 },
+      { name: "Butterscotch Shake", price: 99 },
+      { name: "Chocolate Shake", price: 109 },
+      { name: "Oreo Shake", price: 109 },
+      { name: "Kitkat Shake", price: 109 },
+    ]
+  },
+  {
+    id: 'milk-shakes',
+    name: "Milk Shakes",
+    items: [
+      { name: "Strawberry", price: 59 },
+      { name: "Chocolate", price: 59 },
+      { name: "Butterscotch", price: 59 },
+      { name: "Vanilla", price: 59 },
+      { name: "Oreo", price: 59 },
+      { name: "Mango", price: 59 },
+      { name: "Pista", price: 59 },
+      { name: "Dryfruit", price: 59 },
+    ]
+  },
+  {
+    id: 'bubble-tea',
+    name: "Bubble Shakes & Teas",
+    items: [
+      { name: "Rasamalai Bubble Milk Shake", price: 69 },
+      { name: "Black Current Bubble Milk Shake", price: 69 },
+      { name: "Red Velvet Bubble Milk Shake", price: 69 },
+      { name: "American Bubble Milk Shake", price: 69 },
+      { name: "Kulfi Bubble Milk Shake", price: 69 },
+      { name: "Pineapple Bubble Tea", price: 79 },
+      { name: "Green Melon Bubble Tea", price: 89 },
+      { name: "Hazelnut Bubble Tea", price: 89 },
+    ]
+  },
+  {
+    id: 'mocktails',
+    name: "Mocktails",
+    items: [
+      { name: "Guava Mojito", price: 59 },
+      { name: "Blue Mojito", price: 69 },
+      { name: "Red Wine (Non-Alc)", price: 69 },
+      { name: "Mint Lemon", price: 69 },
+      { name: "Green Apple", price: 69 },
+      { name: "Kiwi Crush", price: 69 },
+    ]
+  },
+  {
+    id: 'bites',
+    name: "Quick Bites",
+    items: [
+      { name: "Plain Maggi", price: 39 },
+      { name: "Egg Maggi", price: 49 },
+      { name: "Paneer Maggi", price: 59 },
+      { name: "Cheese Maggi", price: 59 },
+      { name: "Corn Maggi", price: 59 },
+      { name: "Salted French Fries", price: 69 },
+      { name: "Peri Peri French Fries", price: 79 },
+      { name: "Cheese French Fries", price: 79 },
+      { name: "Masala French Fries", price: 79 },
+    ]
+  },
+  {
+    id: 'snacks',
+    name: "Sandwiches & Snacks",
+    items: [
+      { name: "Veg Sandwich", price: 49 },
+      { name: "Cheese Sandwich", price: 59 },
+      { name: "Paneer Sandwich", price: 59 },
+      { name: "Corn Sandwich", price: 59 },
+      { name: "Chocolate Sandwich", price: 49 },
+      { name: "Osmania Biscuit", price: 5 },
+      { name: "Samosa", price: 15 },
+    ]
+  }
+];
